746997f8ff002c0417825b0447faf6ef3b46f7b4
oleks <oleks@oleks.info>
2017-08-08 15:20:19 +0200

    Remove spurious print statements

--- nisselib.py
+++ nisselib.py
@@ -10,7 +10,6 @@ def is_valid_ssh_key(ssh_key):
     # Each rsa-ssh key has 3 different strings in it, first one being
     # typeofkey second one being keystring third one being username .
     if len(array) != 3:
-        print(array)
         return False
     typeofkey=array[0]
     string=array[1]
@@ -19,14 +18,12 @@ def is_valid_ssh_key(ssh_key):
     try :
         data=base64.decodestring(string)
     except binascii.Error:
-        print('b')
         return False
     a=4
     # unpack the contents of data, from data[:4], it must be equal to 7, property of ssh key .
     try :
         str_len = struct.unpack('>I', data[:4])[0]
     except struct.error :
-        print('c')
         return false
     # data[4:11] must have string which matches with the typeofkey , another ssh key property.
     return data[4:4+str_len] == typeofkey and int(str_len) == int(7)
